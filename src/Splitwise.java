53528eee87a2b778c838f3c1ff0b828b30ff601e
Vaibhaw Raj <vraj@scu.edu>
2019-02-14 12:31:40 -0800

    Initialized Project Structure

---
+++ src/Splitwise.java
@@ -0,0 +1,12 @@
+/**
+ * Splitwise.java : Entry class to initialize User Interface
+
+ * @version		0.1
+ * @since   	2019-02-14
+ */
+
+public class Splitwise {
+	public static void main(String[] arg) {
+		System.out.println("Ok");
+	}
+}
