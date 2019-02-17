006913ee116354a301734ee600e8897cd1c2dfda
Vaibhaw Raj <vaibhaw2020@gmail.com>
2019-02-17 14:35:40 -0800

    Partial update

--- src/splitwisesdk/Http.java
+++ src/splitwisesdk/Http.java
@@ -1,15 +1,26 @@
 package splitwisesdk;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 
 public class Http {
 	static HttpClient client = HttpClientBuilder.create().build();
@@ -41,15 +52,16 @@ public class Http {
 		return responseText;
 	}
 	
-	public static String sendGetRequest(String url, Map<String,String> headers) {
+	public static String sendGetRequest(String url, Map<String,String> params) {
 		String responseText = "";
-		HttpGet request = new HttpGet(url);
+		
 		try {
-			
-			for(Map.Entry<String, String> header : headers.entrySet()) {
-				request.addHeader(header.getKey(),header.getValue());
+			URIBuilder builder = new URIBuilder(url);
+			for(Map.Entry<String, String> param : params.entrySet()) {
+				builder.setParameter(param.getKey(),param.getValue());
 			}
-	
+			HttpGet request = new HttpGet(builder.build());
+			System.out.println(request.toString());
 			HttpResponse response = client.execute(request);
 			BufferedReader rd = new BufferedReader(
 					new InputStreamReader(response.getEntity().getContent()));
--- src/splitwisesdk/auth/OAuth.java
+++ src/splitwisesdk/auth/OAuth.java
@@ -23,60 +23,146 @@ import splitwisesdk.Http;
 public class OAuth {
 	private String consumerKey;
 	private String consumerSecret;
-	private String tokenSecret;
 	private String oauth_token;
-	private String oauth_secret;
+	private String oauth_token_secret;
+	private String oauth_verifier;
 	
 	protected String requestTokenUrl;
 	protected String accessTokenUrl;
 	protected String authorizeUrl;
 	
-	OAuth(String consumerKey, String consumerSecret) {
+	public OAuth(String consumerKey, String consumerSecret) {
 		this.consumerKey = consumerKey;
 		this.consumerSecret = consumerSecret;
-		this.tokenSecret = "";
+		this.oauth_token_secret = "";
 	}
 	
 	/*
 	 * getAuthoriationURL : Returns authorization url which is used to get access
 	 * token
 	 */
-	protected String getAuthorizationURL() {
+	public String getAuthorizationURL() {
 		String authorizationURL = "";
 		
-		// Generate Request
-		OAuthRequest req = new OAuthRequest();
-		req.setConsumerKey(consumerKey);
-		
-		hmac_sha1_sign(req);
-		
-		String body = req.getRequestBody();
-		
-		Map<String, String> headers = new HashMap<String, String>();
-		headers.put("Content-Type","application/x-www-form-urlencoded");
-		
-		String response = Http.sendPostRequest(requestTokenUrl, headers, body);
-		
-		String[] rArr = response.split("&");
-		HashMap<String,String> params = new HashMap<String, String>();
-		
-		for(String r : rArr) {
-			params.put(r.split("=")[0], r.split("=")[1]);
+		if(oauth_token == null) {
+			// Generate Request
+			OAuthRequest req = new OAuthRequest();
+			req.setConsumerKey(consumerKey);
+			req.setEndpoint(requestTokenUrl);
+			req.setMethod("POST");
+			
+			hmac_sha1_sign(req);
+			
+			String body = req.getRequestBody();
+			//System.out.println(body);
+			Map<String, String> headers = new HashMap<String, String>();
+			headers.put("Content-Type","application/x-www-form-urlencoded");
+			
+			String response = Http.sendPostRequest(req.getEndpoint(), headers, body);
+			//System.out.println("Response :" + response);
+			String[] rArr = response.split("&");
+			HashMap<String,String> params = new HashMap<String, String>();
+			//System.out.println("Response " + response);
+			for(String r : rArr) {
+				params.put(r.split("=")[0], r.split("=")[1]);
+				System.out.println(r.split("=")[0] + " " + r.split("=")[1]);
+			}
+			oauth_token = params.get("oauth_token");
+			oauth_token_secret = params.get("oauth_token_secret");
+			//System.out.println("Response: " + response);
 		}
-		oauth_token = params.get("oauth_token");
-		oauth_secret = params.get("oauth_secret");
-		
 		authorizationURL = this.authorizeUrl + "?oauth_token=" + this.oauth_token;
 		return authorizationURL;
 	}
 	
+	public String getAccessToken() {
+		String response = "";
+		
+
+			// Generate Request
+			OAuthRequest req = new OAuthRequest();
+			req.setConsumerKey(consumerKey);
+			req.setOauthToken(oauth_token);
+			req.setOauthTokenSecret(oauth_token_secret);
+			req.setOauthVerifier(oauth_verifier);
+			req.setEndpoint(accessTokenUrl);
+			req.setMethod("POST");
+			hmac_sha1_sign(req);
+			
+			String body = req.getRequestBody();
+			//System.out.println(body);
+			Map<String, String> headers = new HashMap<String, String>();
+			headers.put("Content-Type","application/x-www-form-urlencoded");
+			
+			response = Http.sendPostRequest(req.getEndpoint(), headers, body);
+			System.out.println("Response: " + response);
+			if(response.equalsIgnoreCase("Invalid OAuth Request")) {
+				return "";
+			}
+			String[] rArr = response.split("&");
+			HashMap<String,String> params = new HashMap<String, String>();
+			
+			for(String r : rArr) {
+				params.put(r.split("=")[0], r.split("=")[1]);
+				System.out.println(r.split("=")[0] + " " + r.split("=")[1]);
+			}
+			oauth_token = params.get("oauth_token");
+			oauth_token_secret = params.get("oauth_token_secret");
+		
+		//response = this.authorizeUrl + "?oauth_token=" + this.oauth_token;
+		return response;
+	}
+	
+	public String request(String endpoint) {
+		String response = "";
+		String body_hash = "2jmj7l5rSw0yVb%2FvlWAYkK%2FYBwk%3D";
+		
+		OAuthRequest req = new OAuthRequest();
+		req.setConsumerKey(consumerKey);
+		req.setOauthToken(oauth_token);
+		req.setOauthTokenSecret(oauth_token_secret);
+		req.setEndpoint(endpoint);
+		req.setMethod("GET");
+		req.setOauthBodyHash(body_hash);
+		hmac_sha1_sign(req);
+		
+		response = Http.sendGetRequest(req.getEndpoint() + "?" + req.getRequestBody(), new HashMap<String,String>());
+		return response;
+	}
+	
+	public void setOauthToken(String token) {
+		this.oauth_token = token;
+	}
+	
+	public void setOauthTokenSecret(String token) {
+		this.oauth_token_secret = token;
+	}
+	
+	public void setOauthVerifier(String token) {
+		this.oauth_verifier = token;
+	}
+	
+	public void setRequestTokenURL(String url) {
+		this.requestTokenUrl = url;
+	}
+	
+	public void setAccessTokenURL(String url) {
+		this.accessTokenUrl = url;
+	}
+	
+	public void setAuthorizationURL(String url) {
+		this.authorizeUrl = url;
+	}
+	
 	private void hmac_sha1_sign(OAuthRequest req) {
 		req.setOauthSignatureMethod(OAuthRequest.HMAC_SHA1);
-		String method = "POST";
-		String hashedUrl = URLEncoder.encode(this.requestTokenUrl);
+		String method = req.getMethod();
+		String hashedUrl = URLEncoder.encode(req.getEndpoint());
 		String hashedBody = req.getRequestHash();
 		String signature_base_string = method + "&" + hashedUrl + "&" + hashedBody;
-		String key = consumerSecret + "&" + tokenSecret;
+		String key = consumerSecret + "&" + oauth_token_secret;
+		//System.out.println(key);
+		//System.out.println(signature_base_string);
 		String signature = new String(
 				Base64.encodeBase64(
 						new HmacUtils(
@@ -89,3 +175,16 @@ public class OAuth {
 	}
 	
 }
+
+/*
+ * Normal Get Request
+ * oauth_consumer_key=cCFUP5oGYVNJJAF9PlOR2qqBDcnzzEnPx5hofrh4
+ * oauth_timestamp=1550436114
+ * oauth_nonce=2856831
+ * oauth_version=1.0
+ * oauth_token=Nx9C7NRmGO4VexZ3oc04AzGIJkSwUmA3GWXyMVpl
+ * oauth_body_hash=2jmj7l5rSw0yVb%2FvlWAYkK%2FYBwk%3D
+ * oauth_signature_method=HMAC-SHA1
+ * oauth_signature=YgKRxMCR6ODzhZ7AYTYR73lhBH8%3D
+ * 
+*/
